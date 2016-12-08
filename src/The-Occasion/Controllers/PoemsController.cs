1cd7e783306bf921509c25a3d6707038a5cecbc7
Elizabeth Sanger <libsang@gmail.com>
2016-12-08 11:28:11 -0600

    added single poem view, database initializer, updated schema to include poem collection on poem model

---
+++ src/The-Occasion/Controllers/PoemsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using The_Occasion.Data;
+using The_Occasion.Models;
+
+namespace The_Occasion.Controllers
+{
+    [Authorize]
+    public class PoemsController
+    {
+        
+            private readonly UserManager<ApplicationUser> _userManager;
+
+            private ApplicationDbContext context;
+
+            public PoemsController(ApplicationDbContext ctx, UserManager<ApplicationUser> user)
+            {
+                _userManager = user;
+                context = ctx;
+            }
+}
+}
