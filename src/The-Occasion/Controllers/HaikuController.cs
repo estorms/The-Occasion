ce3d1777e7915c17a44630973798f2b635001090
Elizabeth Sanger <libsang@gmail.com>
2016-12-31 12:18:44 -0600

    restructured haiku/sonnet ctrllers and views

---
+++ src/The-Occasion/Controllers/HaikuController.cs
@@ -0,0 +1,129 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using The_Occasion.Data;
+using The_Occasion.Models;
+using The_Occasion.Models.PoemViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace TheOccasion_Controllers
+{
+    public class HaikuController: Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private ApplicationDbContext context;
+
+        public HaikuController(ApplicationDbContext ctx, UserManager<ApplicationUser> user)
+        {
+            _userManager = user;
+            context = ctx;
+        }
+
+
+        [Authorize]
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> MyHaiku()
+        {
+            //create a new instance of the single poem view model to hold the generated poem
+            //properties to include on single poem view model: 1.poem, 2. linesarray;
+
+            SinglePoemViewModel model = new SinglePoemViewModel(context);
+            var user = await GetCurrentUserAsync();
+            var userName = user.UserName;
+
+            //set some dummy properties on model.Poem so that View doesn't freak out
+
+            //get all the sonnets back from the database
+            var haikus = await context.Poem.Where(p => p.FormId == 120 && (p.Author == "Anselm Hollo" || p.Author == "Udiah (Witness to Yah)")).ToListAsync();
+
+            List<string> HaikuLines = new List<string>();
+            List<string> HaikuSecondLines = new List<string>();
+
+
+            //for each sonnet in the list returned from the database, cycle through, split the lines up into individual arrays
+            foreach (var haiku in haikus)
+            {
+                var haikuLinesArray = Regex.Split(haiku.Lines, "@@");
+                for (int i = 0; i < haikuLinesArray.Length; i++)
+                {
+                    if (i == 1)
+                    {
+                        HaikuSecondLines.Add(haikuLinesArray[i]);
+                    }
+                    else
+                    {
+                        HaikuLines.Add(haikuLinesArray[i]);
+                    }
+                }
+
+            }
+            Random random = new Random();
+
+            var userHaiku = new string[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                int r = random.Next(HaikuLines.Count());
+                //when trying to make userHaiku a list, instead of an array, throws index out of bounds exception right here.
+                userHaiku[i] = HaikuLines[r];
+            }
+
+            //userHaiku = userHaiku.Where((source, index) => index != 1).ToArray();
+
+            model.Line1 = userHaiku[0];
+            model.Line3 = userHaiku[2];
+
+            int r2 = random.Next(HaikuSecondLines.Count());
+            model.Line2 = HaikuSecondLines[r2];
+
+            StringBuilder stringbuilder = new StringBuilder();
+            stringbuilder.Append(userHaiku[0]);
+            stringbuilder.Append("@@");
+            stringbuilder.Append(HaikuSecondLines[r2]);
+            stringbuilder.Append("@@");
+            stringbuilder.Append(userHaiku[2]);
+
+
+
+            Poem myHaiku = new Poem();
+
+            //myHaiku.Title = "Your Computer Writes Better Poetry Than You Do";
+            myHaiku.Author = userName;
+            //need to convert the below to string with stringbuilder as in sonnet and make other changes to razor to allow saving
+            myHaiku.Lines = stringbuilder.ToString();
+            myHaiku.FormId = 120;
+            myHaiku.MoodId = 115;
+            myHaiku.TopicId = 115;
+            model.Poem = myHaiku;
+            //model.LinesArray = userHaiku;
+
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> SaveUserHaiku(Poem poem)
+        {
+            //var user = GetCurrentUserAsync();
+            context.Poem.Add(poem);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ src/The-Occasion/Controllers/SonnetController.cs
@@ -0,0 +1,121 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using The_Occasion.Data;
+using The_Occasion.Models;
+using The_Occasion.Models.PoemViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace TheOccasion_Controllers
+{
+    public class SonnetController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private ApplicationDbContext context;
+
+        public SonnetController(ApplicationDbContext ctx, UserManager<ApplicationUser> user)
+        {
+            _userManager = user;
+            context = ctx;
+        }
+
+
+        [Authorize]
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> MySonnet()
+        {
+            //create a new instance of the single poem view model to hold the generated poem
+            //properties to include on single poem view model: 1.poem, 2. linesarray;
+
+            SinglePoemViewModel model = new SinglePoemViewModel(context);
+            var user = await GetCurrentUserAsync();
+            var userName = user.UserName;
+
+
+            //get all the sonnets back from the database
+            var sonnets = await context.Poem.Where(p => p.FormId == 118).ToListAsync();
+
+            //create a list of strings to hold all the individual lines of each sonnet
+
+            List<string> SonnetLines = new List<string>();
+
+
+            //for each sonnet in the list returned from the database, cycle through, split the lines up into individual arrays and add the lines to the list of sonnet lines
+            foreach (var sonnet in sonnets)
+            {
+                if (sonnet.Lines == null)
+                {
+                    Console.WriteLine("Look at me");
+                }
+                var oneSonnetLinesArr = Regex.Split(sonnet.Lines, "@@");
+                foreach (var line in oneSonnetLinesArr)
+                {
+                    SonnetLines.Add(line);
+                }
+
+            }
+
+            //create a new instance of the random class
+            Random random = new Random();
+
+            //create a new array of strings to hold the user sonnet
+            var UserSonnet = new string[14];
+            //cycle through the list of sonnet lines and insert them at random into the user sonnet array
+
+            for (int i = 0; i < 14; i++)
+            {
+                int r = random.Next(SonnetLines.Count());
+                UserSonnet[i] = SonnetLines[r];
+            }
+
+            StringBuilder stringbuilder = new StringBuilder();
+            foreach (var line in UserSonnet)
+            {
+                stringbuilder.Append(line);
+                stringbuilder.Append("@@");
+            }
+
+            Poem mySonnet = new Poem();
+
+            mySonnet.Lines = stringbuilder.ToString();
+            //mySonnet.Title = "Your Computer Writes Better Poetry Than You Do";
+            mySonnet.Author = userName;
+            //mySonnet.Lines = UserSonnet.ToString();
+            mySonnet.FormId = 118;
+            mySonnet.TopicId = 115;
+            mySonnet.MoodId = 115;
+
+            model.Poem = mySonnet;
+            model.LinesArray = UserSonnet;
+
+            return View(model);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> SaveUserSonnet(Poem poem)
+        {
+            //var user = GetCurrentUserAsync();
+            context.Poem.Add(poem);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
